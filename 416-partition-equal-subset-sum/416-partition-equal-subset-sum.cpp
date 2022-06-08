7ad5738057a89df2719140acba5a0b37def4cc98
Himanshu jain <77209809+donhimanshu15@users.noreply.github.com>
2022-06-08 16:54:15 +0530

    Time: 283 ms (46.46%), Space: 141.1 MB (7.15%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
   bool  equalSubset(int N, vector<int> &arr, vector<vector<int>> & dp, int sum){
     if(sum==0)return 1;
-    if(N==0&& sum!=0) return 0;
+    if(N<0&& sum!=0) return 0;
     if(dp[N][sum]!=-1) return dp[N][sum];
-    if(arr[N-1]>sum) return equalSubset(N-1,arr,dp,sum);
-    return dp[N][sum]= equalSubset(N-1,arr,dp,sum)|| equalSubset(N-1,arr,dp,sum-arr[N-1]);
+    if(arr[N]>sum) return equalSubset(N-1,arr,dp,sum);
+    return dp[N][sum]= equalSubset(N-1,arr,dp,sum)|| equalSubset(N-1,arr,dp,sum-arr[N]);
     
 }
     bool canPartition(vector<int>& arr) {
@@ -15,8 +15,8 @@ public:
             sum+= arr[i];
         }
         if (sum%2!=0) return 0;
-    vector<vector<int> > dp(N + 1,
+    vector<vector<int> > dp(N +1,
                             vector<int>(sum + 1, -1));
-    return equalSubset(N, arr, dp , sum/2);
+    return equalSubset(N-1, arr, dp , sum/2);
     }
 };
