9d689d0727b10a4adf41dd44dfaefab70be4a1e4
Himanshu jain <77209809+donhimanshu15@users.noreply.github.com>
2022-06-13 15:30:59 +0530

    Time: 25 ms (75.00%), Space: 13.5 MB (50.00%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
@@ -5,7 +5,7 @@ public:
         return 0.0;
     double ans = 0;
     
-    //if income is less than first element price, then return (income-0)rate of tax
+   
     if(inc<b[0][0])
          {  
             ans += (b[0][1]== 0)?0:((double)inc*(b[0][1]))/100.0;
@@ -13,7 +13,7 @@ public:
          }
     else 
         ans += (b[0][1]== 0)?0:((double)b[0][0]*(b[0][1]))/100.0;
-    //first price has done, repeat the same process for remaining elements..
+   
     for(int i = 1;i<b.size();i++){
         
         if(b[i][0]<inc){
@@ -22,7 +22,7 @@ public:
         else
         { 
             ans += (b[i][1]==0)?0:((double)(inc-b[i-1][0])*(b[i][1]))/100.0;
-                break;
+            break;
            }
     }
     return ans;
