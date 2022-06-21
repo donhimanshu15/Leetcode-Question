1e0d1ad5520114db8a90edfcbf1132675d98102f
Himanshu jain <77209809+donhimanshu15@users.noreply.github.com>
2022-06-21 16:00:15 +0530

    Added solution - LeetHub

---
+++ LCS of three strings - GFG/lcs-of-three-strings.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3);
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; cin >> n1 >> n2 >> n3;
+        string A, B, C; cin >> A >> B >> C;
+        cout << LCSof3 (A, B, C, n1, n2, n3) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int solve(string A, string B, string C, int n1, int n2, int n3, vector<vector<vector<int>>>&dp){
+   if(n1==0||n2==0||n3==0){
+       return 0;
+   }
+   if(dp[n1][n2][n3]!=-1)return dp[n1][n2][n3];
+   if(A[n1-1]==B[n2-1]&&B[n2-1]==C[n3-1]){
+       return dp[n1][n2][n3]=1+solve(A,B,C,n1-1,n2-1,n3-1,dp);
+   }
+   else{
+       return dp[n1][n2][n3]=max(solve(A,B,C,n1-1,n2,n3,dp),max(solve(A,B,C,n1,n2-1,n3,dp),solve(A,B,C,n1,n2,n3-1,dp)));
+   }
+   return dp[n1][n2][n3];
+}
+int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
+{
+           vector<vector<vector<int>>>dp(n1+1,vector<vector<int>>(n2+1,vector<int>(n3+1,-1)));
+           return solve(A,B,C,n1,n2,n3,dp);
+}
