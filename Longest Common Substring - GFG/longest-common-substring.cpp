86bc446102e87bcef7eab9f81050183616ee8ef2
Himanshu jain <77209809+donhimanshu15@users.noreply.github.com>
2022-06-24 14:32:46 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -5,20 +5,18 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    
-int lcs(int i, int j,string &S1, string &S2,vector<vector<int>>&dp,int &ans)
+    int ans=0;
+int lcs(int i, int j,string &S1, string &S2,vector<vector<int>>&dp)
    {
        int take=0,nottake1=0,nottake2=0;
        if(i<0||j<0) return 0;
        if(dp[i][j]!=-1) return dp[i][j];
        if(S1[i]==S2[j]) 
        {
-       take=1+lcs(i-1,j-1,S1,S2,dp,ans);
+       take=1+lcs(i-1,j-1,S1,S2,dp);
        }
-       
-       
-       nottake1=lcs(i-1,j,S1,S2,dp,ans);
-       nottake2=lcs(i,j-1,S1,S2,dp,ans);
+       nottake1=lcs(i-1,j,S1,S2,dp);
+       nottake2=lcs(i,j-1,S1,S2,dp);
        
        ans=max(ans,take);
        return dp[i][j]=take;
@@ -31,9 +29,9 @@ int lcs(int i, int j,string &S1, string &S2,vector<vector<int>>&dp,int &ans)
    {
        
        vector<vector<int>>dp(n+1, vector<int>(m+1,-1));
-       int ans=0;
        
-       lcs(n-1,m-1,S1,S2,dp,ans);
+       
+       lcs(n-1,m-1,S1,S2,dp);
        return ans;
    }
 };
