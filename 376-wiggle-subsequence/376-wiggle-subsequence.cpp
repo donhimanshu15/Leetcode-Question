fecf89bc5d0cb08c9c8651e003ac089a16d0f899
Himanshu jain <77209809+donhimanshu15@users.noreply.github.com>
2022-07-04 00:07:32 +0530

    Time: 60 ms (5.09%), Space: 16 MB (5.01%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -4,21 +4,21 @@ public:
     int wiggleMaxLength(vector<int>& nums) {
        
         memset(mem,-1,sizeof(mem));
-        return max( dp(1,1,nums[0] ,nums) , dp(1,0,nums[0] ,nums) ) +1;
+        return max( dp(0,1,-1 ,nums) , dp(0,0,-1 ,nums) );
     }
     
     int dp(int i, int high ,int prev,vector<int>& nums ){ 
         if(i==nums.size())return 0; 
-        if(mem[i][high][prev] >-1)return mem[i][high][prev];
+        if(mem[i][high][prev+1] >-1)return mem[i][high][prev+1];
         
         if(high){ 
-            if(nums[i] > prev)return mem[i][high][prev] = max(1 + dp(i+1, 0, nums[i],nums) , dp(i+1,1,prev,nums) ) ;
-            else return mem[i][high][prev] = dp(i+1,1,prev, nums);
+            if(nums[i] > prev or prev==-1)return mem[i][high][prev+1] = max(1 + dp(i+1, 0, nums[i],nums) , dp(i+1,1,prev,nums) ) ;
+            else return mem[i][high][prev+1] = dp(i+1,1,prev, nums);
         }
         
         else{ 
-             if(nums[i] < prev)return mem[i][high][prev] =  max(1+dp(i+1,1,nums[i] , nums) , dp(i+1,0,prev,nums));
-            else return mem[i][high][prev] = dp(i+1,0,prev ,nums);
+             if(nums[i] < prev or prev==-1)return mem[i][high][prev+1] =  max(1+dp(i+1,1,nums[i] , nums) , dp(i+1,0,prev,nums));
+            else return mem[i][high][prev+1] = dp(i+1,0,prev ,nums);
             
         }
         
